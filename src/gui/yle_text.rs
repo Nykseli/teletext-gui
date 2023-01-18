4ec88bc530a3479867d25425818954706cf22ba2
Miika Alikirri <nykseli@protonmail.com>
2023-01-18 14:03:57 +0200

    Draw sub pages and botton navigation

--- src/gui/yle_text.rs
+++ src/gui/yle_text.rs
@@ -1,17 +1,23 @@
-use crate::html_parser::{HtmlItem, HtmlText, TeleText, MIDDLE_TEXT_MAX_LEN};
+use crate::html_parser::{HtmlItem, HtmlLink, HtmlText, TeleText, MIDDLE_TEXT_MAX_LEN};
 use egui::TextStyle;
 
 impl HtmlItem {
     fn add_to_ui(&self, ui: &mut egui::Ui) {
         match self {
             HtmlItem::Link(link) => {
-                if ui.link(&link.inner_text).clicked() {
-                    println!("Clicked {}", link.url);
-                }
+                link.add_to_ui(ui);
             }
             HtmlItem::Text(text) => {
                 ui.label(text);
             }
+        }
+    }
+}
+
+impl HtmlLink {
+    fn add_to_ui(&self, ui: &mut egui::Ui) {
+        if ui.link(&self.inner_text).clicked() {
+            println!("Clicked {}", self.url);
         }
     }
 }
@@ -59,12 +65,42 @@ impl<'a> GuiYleText<'a> {
         }
     }
 
+    fn draw_sub_pages(&mut self, pages: &[HtmlItem]) {
+        let middle_text_start =
+            (self.panel_width / 2.0) - (self.char_width * (MIDDLE_TEXT_MAX_LEN as f32) / 2.0);
+
+        self.ui.horizontal(|ui| {
+            ui.spacing_mut().item_spacing.x = 0.0;
+            ui.add_space(middle_text_start);
+            for item in pages {
+                item.add_to_ui(ui)
+            }
+        });
+    }
+
+    fn draw_bottom_navigation(&mut self, navigation: &[HtmlLink]) {
+        // "Kotimaa | Ulkomaat | Talous | Urheilu | Svenska sidor | Teksti-TV" is 88 chars
+        let page_nav_start = (self.panel_width / 2.0) - (self.char_width * 65.0 / 2.0);
+        self.ui.horizontal(|ui| {
+            ui.spacing_mut().item_spacing.x = 0.0;
+            ui.add_space(page_nav_start);
+            for (idx, item) in navigation.iter().enumerate() {
+                item.add_to_ui(ui);
+                if idx < 5 {
+                    ui.label(" | ");
+                }
+            }
+        });
+    }
+
     pub fn draw(&mut self, page: &TeleText) {
         self.draw_header(&page.title);
         self.draw_page_navigation(&page.page_navigation);
         self.draw_middle(&page.middle_rows);
+        self.draw_sub_pages(&page.sub_pages);
         self.ui.label("\n");
         self.draw_page_navigation(&page.page_navigation);
+        self.draw_bottom_navigation(&page.bottom_navigation);
     }
 
     pub fn new(ui: &'a mut egui::Ui) -> Self {
